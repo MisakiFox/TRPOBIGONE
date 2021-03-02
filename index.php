9d9300950242f8236ef45ecf7ba19ed24edeac37
MisakiFox <misaki.fox.nya@gmail.com>
2021-03-02 09:32:43 +0300

    Lab3

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+namespace laba3;
+
+use Lisovskaya\MyLog;
+
+require_once "core\EquationInterface.php";
+require_once "core\LogAbstract.php";
+require_once "core\LogInterface.php";
+require_once "Lisovskaya\Line.php";
+require_once "Lisovskaya\MyLog.php";
+require_once "Lisovskaya\Quadratic.php";
+MyLog::log("Lis");
+MyLog::write();
