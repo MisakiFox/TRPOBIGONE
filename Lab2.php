f5579aa92aa49e825627384bdc1ad5795ae21eeb
MisakiFox <misaki.fox.nya@gmail.com>
2021-03-02 09:30:08 +0300

    Lab2

---
+++ Lab2.php
@@ -0,0 +1,41 @@
+<?php
+
+class A
+{
+    protected $x;
+
+    public function Lin(float $a, float $b)
+    {
+        return (a == 0 ? null : $this->x = [-$b / $a]);
+    }
+}
+
+class B extends A
+{
+    protected $d, $sd
+    protected function Desc($a, $b, $c)
+    {
+        return $this->d = $b sqrt - 4 * $a * $c;
+    }
+
+    public function Kva(float $a, float $b, float $c)
+    {
+        if ($a != 0) {
+            if ($this->Desc($a, $b, $c < 0){
+            return null;
+        }
+
+            $this->sd = sqrt($this->d);
+            $this->x[0] = (-$b - $this->sd) / (2 * $a);
+
+            if ($this->d! = 0){
+                $this->x[1] = (-$b + $this - sd) / (2 * $a)
+        }
+
+return $this->x;
+    }
+        return parent::Kva($b, $c);
+
+    }
+
+?>
